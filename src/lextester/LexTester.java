cd6e1a87b5d75c2341b834c8363b33f73d7e9c93
Kyle Wu <kylewu2@gmail.com>
2018-09-28 17:54:01 -0700

    Added output file functionality. Created an output folder for token text files.

--- src/lextester/LexTester.java
+++ src/lextester/LexTester.java
@@ -1,42 +1,61 @@
 /**
  *  Author: Kyle Wu
  *  This program takes user input to specify which file to search for tokens in the A4 language.
- *  The tokens are displayed as output, along with the number of tokens in the program.
+ *  The output is the tokens identified in string form, the number of tokens in the file, and
+ *  a text file containing the same list of string-form tokens.
  */
 
 import java.util.*;
 import java.io.*;
 
 public class LexTester {
+    /**
+     *  Outputs a text file containing the tokens for a given list of tokens
+     * @param tl            The array list of tokens
+     * @param fileName      The name of the output file
+     * @throws FileNotFoundException
+     */
+    public static void createTokenFile(ArrayList<Token> tl, String fileName) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter( String.format(".\\Output Token Files\\%s", fileName) );
+        for (Token t : tl)
+            writer.println(t.toString());
+        writer.close();
+    }
+
     public static void main(String[] args) {
         try {
             String program;
             if (args.length > 0) {
+                int count = 1;
+
                 for (String arg : args) {
+                    Lexer l = new Lexer();
                     program = arg;
 
-                    Lexer l = new Lexer();
                     l.tokenize(program);
+                    createTokenFile(l.getTokens(), String.format("tokens%d.txt", count));
                     l.displayTokens();
                     System.out.printf("\n%s contains: %d tokens.\n", program, l.getTokens().size());
                     System.out.println("________________________________________________");
+
+                    count++;
                 }
             }
 
             else {
                 Lexer l = new Lexer();
-
                 Scanner input = new Scanner(System.in);
+
                 System.out.print("Enter the name of the file to tokenize:\n> ");
                 program = input.nextLine();
                 input.close();
-                
+
                 l.tokenize(program);
+                createTokenFile(l.getTokens(), "tokens.txt");
                 l.displayTokens();
                 System.out.printf("\n%s contains: %d tokens.\n", program, l.getTokens().size());
                 System.out.println("________________________________________________");
             }
-
         }
         catch (FileNotFoundException f) {}
         catch (NullPointerException a) { System.out.println("Error - Null pointer."); }
